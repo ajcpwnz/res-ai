83d4cf2f98f3c67b3e6936b961fb06a1fc3dbfdf
alex <ajcpwnz@d3feat.website>
2025-06-03 17:03:25 +0300

    t: 11

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -20,7 +20,7 @@ module.exports = {
       repo: "git@github.com:ajcpwnz/res-ai.git",
       path: "/root/res-ai",
       keep_releases: 0,
-      "post-deploy": "/bin/bun install && pm2 start ecosystem.config.cjs"
+      "post-deploy": "/root/.bun/bin/bun install && pm2 start ecosystem.config.cjs"
     }
   }
 };
